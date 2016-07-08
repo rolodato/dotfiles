4f3c5ea91d7ca8b3e810f871149b947dff17e7f4
Rodrigo López Dato <rolodato@rolodato.com>
2016-07-08 03:16:16 -0300

    Add config.nix file with dependencies

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,3 +1,24 @@
 {
-  allowUnfree = true;
+  packageOverrides = defaultPkgs: with defaultPkgs; {
+    rolodato = with pkgs; buildEnv {
+      name = "rolodato";
+      paths = [
+        "coreutils"
+        "curl"
+        "ffmpeg"
+        "git"
+        "gnupg"
+        "jq"
+        "keychain"
+        "nss-cacert"
+        "oh-my-zsh-git-2016-04-20"
+        "openssh-7"
+        "pinentry"
+        "stow"
+        "vim"
+        "wget"
+        "zsh"
+      ];
+    };
+  };
 }
