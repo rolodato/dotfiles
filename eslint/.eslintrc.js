b7e6414453f0221ba14e8aa0519bcf65b4b9307a
Rodrigo López Dato <rolodato@rolodato.com>
2016-07-09 12:56:52 -0300

    ESLint

---
+++ eslint/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "rules": {
+        "no-trailing-spaces": "error",
+        "semi": "error",
+        "indent": ["error", 4]
+    }
+};
